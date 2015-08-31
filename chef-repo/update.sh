c701753aad75f2672ae040dac02561fdf7041d4a
sheldonabrown <sheldonb@gmail.com>
2015-08-31 06:24:27 -0400

    tweaking prediction storage value

--- chef-repo/update.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash 
-ssh -i ~/.ec2/obawmata_dev.pem ubuntu@$1 -n "\
-cd wmata-devops && \
-git pull && \
-cd .. && \
-sudo chef-solo -l debug -L /tmp/chef.log -c wmata-devops/chef-repo/solo.rb -E dev -j wmata-devops/chef-repo/$2.json"
