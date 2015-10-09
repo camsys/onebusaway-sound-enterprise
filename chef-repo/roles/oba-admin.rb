fdddf8baa287a34164e21462e7b93bba0c9a7119
sheldonabrown <sheldonb@gmail.com>
2015-10-09 14:52:59 -0400

    adding watchdog support

--- chef-repo/roles/oba-admin.rb
+++ chef-repo/roles/oba-admin.rb
@@ -10,7 +10,7 @@ override_attributes(:maven => {
                       :m2_home => '/var/lib/maven'
                     },
                     :tomcat => {
-                      :java_options => '-Xmx3G -Xms1G -XX:MaxPermSize=256m -Djava.awt.headless=true -XX:+UseConcMarkSweepGC'
+                      :java_options => '-Xmx3G -Xms1G -XX:MaxPermSize=256m -Djava.awt.headless=true -XX:+UseConcMarkSweepGC',
                       :instances => {
                         :watchdog => {
                           'port' => 7070,
