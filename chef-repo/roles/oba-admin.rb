d985baf48e256ee43a00331ef6536abb68f92e3e
sheldonabrown <sheldonb@gmail.com>
2015-10-11 16:27:24 -0400

    changing watchdog to use remote TDS

--- chef-repo/roles/oba-admin.rb
+++ chef-repo/roles/oba-admin.rb
@@ -10,13 +10,6 @@ override_attributes(:maven => {
                       :m2_home => '/var/lib/maven'
                     },
                     :tomcat => {
-                      :java_options => '-Xmx3G -Xms1G -XX:MaxPermSize=256m -Djava.awt.headless=true -XX:+UseConcMarkSweepGC',
-                      :instances => {
-                        :watchdog => {
-                          'port' => 7070,
-                          'ajp_port' => 7009,
-                          'shutdown_port' => 7005
-                        }
-                      }
+                      :java_options => '-Xmx3G -Xms1G -XX:MaxPermSize=256m -Djava.awt.headless=true -XX:+UseConcMarkSweepGC'
                     }
 )
