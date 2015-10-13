cb255fef7e47440743f025d367317e7374c983ba
sheldonabrown <sheldonb@gmail.com>
2015-10-13 07:53:18 -0400

    changes to support qa env

--- chef-repo/environments/qa.rb
+++ chef-repo/environments/qa.rb
@@ -1,6 +1,6 @@
 require 'chef/data_bag'
-name "dev"
-description "environment attributes/configuration for dev environment"
+name "qa"
+description "environment attributes/configuration for qa environment"
 default_attributes({
                        "oba" => {
                        "user" => "ubuntu",
@@ -15,7 +15,7 @@ default_attributes({
                                "repositories" => ["http://developer.onebusaway.org/archiva/repository/snapshots/"]
 
                        },
-                       "db_master" => "db.dev.wmata.obaweb.org",
+                       "db_master" => "db.qa.wmata.obaweb.org",
                        "db_user" => "oba",
                        "db_password" => "changeme",
                        "env" => "dev",
@@ -23,9 +23,9 @@ default_attributes({
                        "db_instance_name" => "org_onebusaway_users",
                        "db_agency" => "gtfsrt",
                        "db_archive" => "gtfsrt",
-                       "api_server" => "app.dev.wmata.obaweb.org:8080",
-                       "admin_server" => "admin.dev.wmata.obaweb.org",
-                       "prediction_api_server" => "gtfsrt.dev.wmata.obaweb.org",
+                       "api_server" => "app.qa.wmata.obaweb.org:8080",
+                       "admin_server" => "admin.qa.wmata.obaweb.org",
+                       "prediction_api_server" => "gtfsrt.qa.wmata.obaweb.org",
                        "admin_server_port" => "8080",
                        "tds" => {
                          "bundle_path" => "/var/lib/oba/bundle"
@@ -33,26 +33,23 @@ default_attributes({
                        "webapp" => {
                          "artifact" => "onebusaway-enterprise-acta-webapp"
                        },
+                       "transitime" => {
+                         "dbhost" => "db.qa.wmata.obaweb.org:3306",
+                         "dbtype" => "mysql",
+                         "dbusername" => "transitime",
+                         "dbpassword" => "transitimeqa",
+                         "dbname" => "transitime",
+                         "agency" => "1",
+                         "api_key" => "qa3273b0",
+                         "encryptionPassword" => "transitimeqa",
+                         "sqsUrl" => "https://sqs.us-east-1.amazonaws.com/372394388595/obawmata_qa",
+                         "sqsKey" => "AKIAJTZPOKKDCF3ZLW4Q",
+                         "sqsSecret" => "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"
+                       }
                      },
-                     "transitime" => {
-                        "dbhost" => "db.dev.wmata.obaweb.org:3306",
-                        "dbtype" => "mysql",
-                        "dbusername" => "prediction",
-                        "dbpassword" => "changeme",
-                        "dbname" => "transitime",
-                        "agency" => "1",
-                        "api_key" => "4b248c1b",
-                        "encryptionPassword" => "SET THIS!",
-                        "sqsUrl" => "https://sqs.us-east-1.amazonaws.com/443046490497/obawmata_prod",
-                        "sqsKey" => "AKIAJD2S6RVL5BB5VD6Q",
-                        "sqsSecret" => "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",
-                        "snsKey" => "AKIAJ34CZNNFNL5G2CUA",
-                        "snsSecret" => "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",
-                        "snsArn" => "arn:aws:sns:us-east-1:443046490497:wmata_avl"
-                      },
                      "aws" => {
-                       "cloudwatch_publish_key" => "AKIAJ2USYJ54JLTDL6CA",
-                       "cloudwatch_publish_secret" => "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",
+                       "cloudwatch_publish_key" => "AKIAIHDQDZCGSQMYJAHQ",
+                       "cloudwatch_publish_secret" => "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",
                        "cloudwatch_endpoint" => "monitoring.us-east-1.amazonaws.com"
                      },
                      "tomcat" => {
