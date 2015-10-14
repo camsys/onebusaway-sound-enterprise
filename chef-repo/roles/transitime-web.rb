74c16eb8874be6abd77ab973d555818c83ecfef5
sheldonabrown <sheldonb@gmail.com>
2015-10-14 09:46:15 -0400

    updates for qa env

--- chef-repo/roles/transitime-web.rb
+++ chef-repo/roles/transitime-web.rb
@@ -10,6 +10,6 @@ override_attributes(:maven => {
                       :m2_home => '/var/lib/maven'
                     },
                     :tomcat => {
-                      :java_options => '-Xmx3G -Xms1G -XX:MaxPermSize=256m -Djava.awt.headless=true -XX:+UseConcMarkSweepGC -Dtransitime.rmi.timeoutSec=300'
+                      :java_options => '-Xmx3G -Xms1G -XX:MaxPermSize=256m -Djava.awt.headless=true -XX:+UseConcMarkSweepGC -Dtransitime.rmi.timeoutSec=300 -Dtransitime.db.encryptionPassword=transitimeqa'
                     }
 )
