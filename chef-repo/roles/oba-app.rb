05b80a2c7e904f3427e3cea36b86bde23e35bd12
Lenny Caraballo <lennycaraballo@gmail.com>
2015-11-02 16:03:45 -0500

    Add Cloudwatch_Monitoring cookbook to oba-app

--- chef-repo/roles/oba-app.rb
+++ chef-repo/roles/oba-app.rb
@@ -3,7 +3,8 @@ description "oba app/front-end server"
 run_list(
         "role[base]",
         "recipe[tomcat]",
-        "recipe[oba::app]"
+        "recipe[oba::app]",
+        "recipe[cloudwatch_monitoring]"
 )
 
 override_attributes(
@@ -13,5 +14,12 @@ override_attributes(
                     },
                     :tomcat => {
                       :java_options => '-Xmx3G -Xms1G -XX:MaxPermSize=256m -Djava.awt.headless=true -XX:+UseConcMarkSweepGC'
+                    },
+                    :cw_mon => {
+                      :version => '1.2.1',
+                      :home_dir => '/var/lib/oba/monitoring/aws-scripts-mon',
+                      :user => 'ubuntu',
+                      :group => 'ubuntu',
+                      :options => '/var/lib/oba/monitoring/aws-scripts-mon/mon-put-instance-data.pl --mem-util --mem-used --mem-avail --disk-path=/ --disk-space-util --disk-space-used --disk-space-avail'
                     }
 )
