83da3141f29fe8a7456300bc240a184d79a72666
lcaraballo <lennycaraballo@gmail.com>
2015-11-03 09:21:37 -0500

    Adding monitoring to other servers

--- chef-repo/roles/transitime-core.rb
+++ chef-repo/roles/transitime-core.rb
@@ -9,5 +9,13 @@ override_attributes(
                     :tz => 'America/New_York',
                     :maven => {
                       :m2_home => '/var/lib/maven'
+                    },
+                    :cw_mon => {
+                      :version => '1.2.1',
+                      :cron_min_freq => '3',
+                      :home_dir => '/var/lib/oba/monitoring',
+                      :user => 'ubuntu',
+                      :group => 'ubuntu',
+                      :options => %w{--mem-util --mem-used --mem-avail --disk-path=/ --disk-space-util --disk-space-used --disk-space-avail}
                     }
 )
--- chef-repo/roles/transitime-web.rb
+++ chef-repo/roles/transitime-web.rb
@@ -6,5 +6,13 @@ run_list(
         "recipe[transitime::web]"
 )
 override_attributes(
-                    :tz => 'America/New_York'
+                    :tz => 'America/New_York',
+                    :cw_mon => {
+                      :version => '1.2.1',
+                      :cron_min_freq => '3',
+                      :home_dir => '/var/lib/oba/monitoring',
+                      :user => 'ubuntu',
+                      :group => 'ubuntu',
+                      :options => %w{--mem-util --mem-used --mem-avail --disk-path=/ --disk-space-util --disk-space-used --disk-space-avail}
+                    }
 )
