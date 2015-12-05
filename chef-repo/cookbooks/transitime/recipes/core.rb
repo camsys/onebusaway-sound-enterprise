9b782357fb9fefd03fd455170c088975a2802f56
sheldonabrown <sheldonb@gmail.com>
2015-12-05 14:11:22 -0500

    fixing tmp permissions

--- chef-repo/cookbooks/transitime/recipes/core.rb
+++ chef-repo/cookbooks/transitime/recipes/core.rb
@@ -1,9 +1,9 @@
 mvn_version = node[:oba][:mvn][:version_transitime_web]
-mvn_dest_file = "/tmp/transitimeCore-#{mvn_version}-onejar.jar"
+mvn_dest_file = "/tmp/chef/transitimeCore-#{mvn_version}-onejar.jar"
 log "maven dependency installed at #{mvn_dest_file}"
 maven "transitimeCore" do
   group_id "transitime"
-  dest "/tmp"
+  dest "/tmp/chef/"
   version mvn_version
   packaging "jar"
   classifier "onejar"
