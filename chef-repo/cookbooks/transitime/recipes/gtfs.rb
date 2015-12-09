8995b6597bd5dca06be976f3f0f15a9498a354de
sheldonabrown <sheldonb@gmail.com>
2015-12-09 16:14:23 -0500

    fixing tmp dir permission issues

--- chef-repo/cookbooks/transitime/recipes/gtfs.rb
+++ chef-repo/cookbooks/transitime/recipes/gtfs.rb
@@ -1,9 +1,9 @@
 mvn_version = node[:oba][:mvn][:version_transitime_web]
-mvn_dest_file = "/tmp/transitimeCore-#{mvn_version}-processGTFSFile.jar"
+mvn_dest_file = "/tmp/chef/transitimeCore-#{mvn_version}-processGTFSFile.jar"
 log "maven dependency installed at #{mvn_dest_file}"
 maven "transitimeCore" do
   group_id "transitime"
-  dest "/tmp"
+  dest "/tmp/chef"
   version mvn_version
   packaging "jar"
   classifier "processGTFSFile"
