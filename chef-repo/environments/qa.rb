ef38cd671c9054f5312141990e44ec8f9ca54b78
sheldonabrown <sheldonb@gmail.com>
2016-02-11 14:54:29 -0500

    bumping dev, qa

--- chef-repo/environments/qa.rb
+++ chef-repo/environments/qa.rb
@@ -7,11 +7,11 @@ default_attributes({
                        "home" => "/home/ubuntu",
                        "mvn" => {
                            "group_id" => "org.onebusaway",
-                           "version_admin" => "1.1.15.5-cs",
-                           "version_core" => "1.1.15.5-cs",
-                           "version_app" => "1.1.15.5-cs",
-                           "version_transitime_core" => "0.0.5",
-                           "version_transitime_web" => "0.0.5",
+                           "version_admin" => "1.1.15.6-cs",
+                           "version_core" => "1.1.15.6-cs",
+                           "version_app" => "1.1.15.6-cs",
+                           "version_transitime_core" => "0.0.6",
+                           "version_transitime_web" => "0.0.6",
                            "repositories" => ["http://repo.obaweb.org:8080/archiva/repository/releases/"]
                        },
                        "db_instance" => "db",
