0b00e7dc6f696bbb343a5f7ea48ff70a9683a14d
sheldonabrown <sheldonb@gmail.com>
2016-03-11 04:21:16 -0500

    bumping prod

--- chef-repo/environments/prod.rb
+++ chef-repo/environments/prod.rb
@@ -7,11 +7,11 @@ default_attributes({
                        "home" => "/home/ubuntu",
                        "mvn" => {
                            "group_id" => "org.onebusaway",
-                           "version_admin" => "1.1.15.4-cs",
-                           "version_core" => "1.1.15.4-cs",
-                           "version_app" => "1.1.15.4-cs",
-                           "version_transitime_core" => "0.0.4",
-                           "version_transitime_web" => "0.0.4",
+                           "version_admin" => "1.1.15.15-cs",
+                           "version_core" => "1.1.15.15-cs",
+                           "version_app" => "1.1.15.15-cs",
+                           "version_transitime_core" => "0.0.15",
+                           "version_transitime_web" => "0.0.15",
                            "repositories" => ["http://repo.prod.wmata.obaweb.org:8080/archiva/repository/releases/"]
                        },
                        "db_instance" => "db",
