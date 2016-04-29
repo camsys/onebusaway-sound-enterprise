c1d9974ac79cfaa620f252bf944070b8ff154434
sheldonabrown <sheldonb@gmail.com>
2016-04-29 06:38:33 -0400

    bug fix for admin

--- chef-repo/roles/oba-admin.rb
+++ chef-repo/roles/oba-admin.rb
@@ -4,8 +4,8 @@ run_list(
         "role[base]",
         "recipe[apache2]",
 	"recipe[apache2::mod_proxy_http]",
-        "recipe[tomcat-apache-mod-jk]",
         "recipe[tomcat]",
+        "recipe[tomcat-apache-mod-jk]",
         "recipe[oba::admin]",
         "recipe[transitime::gtfs]"
 )
