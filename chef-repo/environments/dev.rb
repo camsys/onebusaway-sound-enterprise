70b35f0a480cd32e766ceacb50db523b1c005ae0
sheldonabrown <sheldonb@gmail.com>
2016-08-10 09:16:54 -0400

    bumping dev

--- chef-repo/environments/dev.rb
+++ chef-repo/environments/dev.rb
@@ -7,10 +7,10 @@ default_attributes({
                        "home" => "/home/ubuntu",
                        "mvn" => {
                            "group_id" => "org.onebusaway",
-                           "version_admin" => "1.1.15.31-cs-SNAPSHOT",
-                           "version_core" => "1.1.15.31-cs-SNAPSHOT",
-                           "version_app" => "1.1.15.31-cs-SNAPSHOT",
-                           "version_branded" => "1.1.15.31-cs-SNAPSHOT",
+                           "version_admin" => "1.1.15.32-cs-SNAPSHOT",
+                           "version_core" => "1.1.15.32-cs-SNAPSHOT",
+                           "version_app" => "1.1.15.32-cs-SNAPSHOT",
+                           "version_branded" => "1.1.15.32-cs-SNAPSHOT",
                            "version_transitime_core" => "0.0.31-SNAPSHOT",
                            "version_transitime_web" => "0.0.31-SNAPSHOT",
                            "repositories" => ["http://repo.obaweb.org:8080/archiva/repository/snapshots/"]
