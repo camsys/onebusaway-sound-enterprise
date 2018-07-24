bb14574587b06a3018f0491b82859b11b36eb7b1
sheldonabrown <sheldonb@gmail.com>
2018-07-24 08:52:20 -0400

    adding more qa env

--- chef-repo/environments/qa.rb
+++ chef-repo/environments/qa.rb
@@ -35,6 +35,9 @@ default_attributes({
                        "webapp" => {
                          "artifact" => "onebusaway-enterprise-wmata-webapp"
                        },
+		       "wmata_webapp" => {
+                          "artifact" => "onebusaway-enterprise-wmata-webapp"
+                       },
                        "ses_host" => "email-smtp.us-east-1.amazonaws.com",
                        "ses_user" => "AKIAISKUXW2UHBZRHHNA",
                        "ses_password" => "AhxzNCmnlqzK8qjPwsQ41yHUbk3meOlHZvVRuoVoM7/t",
