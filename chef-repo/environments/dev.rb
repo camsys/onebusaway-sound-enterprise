c5164bb6a561f48f730d29a5eab2a6e3e005bdf7
sheldonabrown <sheldonb@gmail.com>
2021-01-01 16:49:07 -0500

    bumping dev shuttle

--- chef-repo/environments/dev.rb
+++ chef-repo/environments/dev.rb
@@ -14,8 +14,8 @@ default_attributes({
                            "version_branded" => "2.0.72-cs-SNAPSHOT",
                            "version_transitime_core" => "0.0.43-SNAPSHOT",
                            "version_transitime_web" => "0.0.43-SNAPSHOT",
-                           "version_shuttle_transitime_core" => "2.0.38-rpts",
-                           "version_shuttle_transitime_web" => "2.0.42-rpts",
+                           "version_shuttle_transitime_core" => "2.0.38-cs-SNAPSHOT",
+                           "version_shuttle_transitime_web" => "2.0.38-cs-SNAPSHOT",
                            "repositories" => ["https://repo.camsys-apps.com/snapshots/"]
                        },
                        "db_instance" => "db",  
